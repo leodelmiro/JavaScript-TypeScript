b6462e86de1fcbed1c5b29130568ddfcd784b7cc
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-09-15 13:45:24 -0300

    feat: basic js const

---
+++ javascript/aula3/index.js
@@ -0,0 +1,7 @@
+const firstName = 'Leonardo'
+const age = 27
+const weight = 69
+const height = 1.73
+const imc = weight / (height ^ 2)
+
+console.log(`${firstName} is ${age} years old, weight: ${weight} kg, height: ${height} so IMC is ${imc}`)
