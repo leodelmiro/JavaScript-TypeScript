9669d9d0bbe615716d021cf251fba96403e731cf
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-11-13 18:16:53 -0300

    feat: exercicio de validacao de formulario

--- javascript/aula17/assets/js/main.js
+++ javascript/aula17/assets/js/main.js
@@ -1,29 +1,35 @@
-function limparCPF(cpf) {
-  return cpf.replace(/\D+/g, '');
+class ValidaCpf {
+  constructor(cpf) {
+    this.cpf = cpf;
+    this.cpfLimpo = this.limparCPF(cpf);
+  }
+
+  limparCPF(cpf) {
+    return cpf.replace(/\D+/g, '');
+  }
+
+  calcularDigito(cpfArray, fator) {
+    const total = cpfArray
+      .slice(0, fator - 1)
+      .reduce((ac, digit, idx) => ac + Number(digit) * (fator - idx), 0);
+    const digito = 11 - (total % 11);
+    return digito > 9 ? 0 : digito;
+  }
+
+  validar() {
+    if (this.cpfLimpo.length !== 11) return false;
+
+    const cpfArray = Array.from(this.cpfLimpo);
+    const primeiroDigito = this.calcularDigito(cpfArray, 10);
+    const segundoDigito = this.calcularDigito([...cpfArray.slice(0, 9), primeiroDigito], 11);
+
+    return (
+      primeiroDigito === Number(cpfArray[9]) &&
+      segundoDigito === Number(cpfArray[10])
+    );
+  }
 }
 
-function calcularDigito(cpfArray, fator) {
-  const total = cpfArray
-    .slice(0, fator - 1)
-    .reduce((ac, digit, idx) => ac + Number(digit) * (fator - idx), 0);
-  const digito = 11 - (total % 11);
-  return digito > 9 ? 0 : digito;
-}
-
-function validarCPF(cpf) {
-  const cpfLimpo = limparCPF(cpf);
-  if (cpfLimpo.length !== 11) return false;
-
-  const cpfArray = Array.from(cpfLimpo);
-  const primeiroDigito = calcularDigito(cpfArray, 10);
-  const segundoDigito = calcularDigito([...cpfArray.slice(0, 9), primeiroDigito], 11);
-
-  return (
-    primeiroDigito === Number(cpfArray[9]) &&
-    segundoDigito === Number(cpfArray[10])
-  );
-}
-
-// Exemplo de uso:
 const cpf = '705.484.450-52';
-console.log(validarCPF(cpf)); // true
+const validador = new ValidaCpf(cpf);
+console.log(validador.validar());
