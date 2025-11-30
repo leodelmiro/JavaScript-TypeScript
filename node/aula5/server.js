f54b5eff314a5299cc9ba5207bf368605b7cfe0f
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-11-30 00:10:13 -0300

    feat: exercise express + webpack

---
+++ node/aula5/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+const path = require('path')
+
+// Middleware para ler corpo de formularios (POST)
+app.use(express.urlencoded({ extended: true }));
+// Referenciando arquivos staticos
+app.use(express.static(path.resolve(__dirname, 'public')));
+// Middleware para JSON (POST)
+app.use(express.json());
+//  Set para usar as views, não precisa usar path, pode usar caminho absoluto.
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+
+app.use(routes)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
