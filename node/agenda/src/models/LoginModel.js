ac7bf28d5cd43c8f24eac3b43496ed589435f98f
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-12-05 15:22:37 -0300

    feat: Agenda - validação de usuário ja existente e bcrypt para salvar senha

--- node/agenda/src/models/LoginModel.js
+++ node/agenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -18,11 +19,24 @@ class Login {
     async register() {
         this.valida();
         if (this.errors.length > 0) return
+
+        await this.userExists()
+        if (this.errors.length > 0) return
+
+        const salt = bcryptjs.genSaltSync()
+        this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
         try {
             this.user = await loginModel.create(this.body)
         } catch {
             console.log(e)
         }
+    }
+
+    async userExists() {
+        const user = await loginModel.findOne({ email: this.body.email })
+
+        if (user) this.errors.push("Usuário já existe!")
     }
 
     valida() {
