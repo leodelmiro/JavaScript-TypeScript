ed842fa069e553e019e97f15ffae96381f7f772a
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-12-16 20:41:26 -0300

    feat: Agenda - Editando contato

--- node/agenda/src/controllers/contatoController.js
+++ node/agenda/src/controllers/contatoController.js
@@ -39,3 +39,29 @@ exports.editIndex = async function (req, res) {
     });
     return
 };
+
+
+exports.edit = async function (req, res) {
+    try {
+        if (!req.params.id) return res.render('404')
+
+        const contato = new Contato(req.body)
+        await contato.edit(req.params.id)
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors)
+            req.session.save(function () {
+                const backURL = req.header('Referer') || '/contato';
+                res.redirect(backURL);
+            })
+            return
+        }
+
+        req.flash('success', 'Contato editado com sucesso')
+        req.session.save(() => res.redirect(`/contato/${contato.contato._id}`))
+        return
+    } catch (e) {
+        console.log(e)
+        return res.render('404')
+    }
+};
