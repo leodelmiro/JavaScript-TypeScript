61a6d21cfd820da8aec2291654cfa61528a30e4a
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-12-16 21:00:54 -0300

    feat: Agenda - Apagando e Listando contatos

--- node/agenda/src/controllers/contatoController.js
+++ node/agenda/src/controllers/contatoController.js
@@ -65,3 +65,17 @@ exports.edit = async function (req, res) {
         return res.render('404')
     }
 };
+
+exports.delete = async function (req, res) {
+    if (!req.params.id) return res.render('404')
+
+    const contato = await Contato.delete(req.params.id)
+    if (!contato) return res.render('404')
+
+    req.flash('success', 'Contato deletado com sucesso')
+    req.session.save(function () {
+        const backURL = req.header('Referer') || '/';
+        res.redirect(backURL);
+    })
+    return
+};
