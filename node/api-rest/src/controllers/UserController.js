f264be24f50694ce710f5551cfde689233c29eb8
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-12-22 16:27:06 -0300

    feat: API Rest - CRUD usuario

--- node/api-rest/src/controllers/UserController.js
+++ node/api-rest/src/controllers/UserController.js
@@ -4,9 +4,97 @@ class UserController {
   async create(req, res) {
     try {
       const novoUser = await User.create(req.body)
-      res.json(novoUser)
+      return res.json(novoUser)
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map(err => err.message) })
+      return res.status(400).json({ errors: e.errors.map(err => err.message) })
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll()
+      return res.json(users)
+    } catch (e) {
+      console.log(e)
+      return res.json(null)
+    }
+  }
+
+  async get(req, res) {
+    try {
+      const { id } = req.params
+      const user = await User.findByPk(id)
+      return res.json(user)
+    } catch (e) {
+      console.log(e)
+      return res.status(404)
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.']
+        })
+      }
+
+      const user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.']
+        })
+      }
+
+      if (req.body.email && req.body.email !== user.email) {
+        const emailUsed = await User.findOne({ where: { email: req.body.email } })
+        if (emailUsed) {
+          return res.status(400).json({ errors: ['Email já existe'] })
+        }
+      }
+
+      const usuarioAtualizado = await user.update(req.body)
+
+      return res.json(usuarioAtualizado)
+    } catch (e) {
+      console.log(e)
+      if (e && e.errors) {
+        return res.status(400).json({ errors: e.errors.map(err => err.message) })
+      }
+      return res.status(500).json({ errors: ['Erro interno'] })
+    }
+  }
+
+    async delete(req, res) {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.']
+        })
+      }
+
+      const user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.']
+        })
+      }
+
+      await user.destroy()
+
+      return res.status(204).json()
+    } catch (e) {
+      console.log(e)
+      if (e && e.errors) {
+        return res.status(400).json({ errors: e.errors.map(err => err.message) })
+      }
+      return res.status(500).json({ errors: ['Erro interno'] })
     }
   }
 }
--- node/api-rest/src/routes/user.js
+++ node/api-rest/src/routes/user.js
@@ -3,5 +3,9 @@ import userController from '../controllers/UserController'
 const router = new Router()
 
 router.post('/', userController.create)
+router.get('/', userController.index)
+router.get('/:id', userController.get)
+router.put('/:id', userController.update)
+router.delete('/:id', userController.delete)
 
 export default router
