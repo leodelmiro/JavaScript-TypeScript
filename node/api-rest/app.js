ca8a1648c300edfac1b96c7f0855f84df4f7f686
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-12-29 21:37:02 -0300

    feat: API REST - Adicionando statics files

--- node/api-rest/app.js
+++ node/api-rest/app.js
@@ -6,6 +6,8 @@ import aluno from './src/routes/aluno'
 import foto from './src/routes/foto'
 import dotenv from 'dotenv'
 
+import { resolve } from 'path'
+
 dotenv.config()
 
 import './src/database'
@@ -20,6 +22,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
+    this.app.use(express.static(resolve(__dirname, 'uploads')))
   }
 
   routes() {
